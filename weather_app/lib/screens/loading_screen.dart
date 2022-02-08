140da1078ce4869e45af96cbf85c440661b9f4ee
Art b <94374097+artb433@users.noreply.github.com>
2022-02-08 13:58:04 +0000

    his

--- weather_app/lib/screens/loading_screen.dart
+++ weather_app/lib/screens/loading_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:weather_app/services/location.dart';
 import 'package:http/http.dart' as http;
 
@@ -9,10 +10,37 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
   void getPosition() async {
-    Location location = Location();
-    location.getCurrentLocation();
-    print(location.latitude);
-    print(location.longitude);
+    Future<Position> _determinePosition() async {
+      bool serviceEnabled;
+      LocationPermission permission;
+      serviceEnabled = await Geolocator.isLocationServiceEnabled();
+      if (!serviceEnabled) {
+        await Geolocator.openLocationSettings();
+        return Future.error('Location services are disabled.');
+      }
+      permission = await Geolocator.checkPermission();
+      if (permission == LocationPermission.denied) {
+        permission = await Geolocator.requestPermission();
+        if (permission == LocationPermission.denied) {
+          return Future.error('Location permissions are denied');
+        }
+      }
+      if (permission == LocationPermission.deniedForever) {
+        return Future.error(
+            'Location permissions are permanently denied, we cannot request permissions.');
+      }
+      return await Geolocator.getCurrentPosition(
+          forceAndroidLocationManager: false,
+          desiredAccuracy: LocationAccuracy.best);
+    }
+
+    Position position = await _determinePosition();
+
+    // Location location = Location();
+    // location.getCurrentLocation();
+    print('latitude: ${position.latitude}');
+    print('longitude : ${position.longitude}');
+    print(position);
   }
 
   void getData() async {
