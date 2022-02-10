471ae0641d8d68959ede247b968358a5d13303cd
Art b <94374097+artb433@users.noreply.github.com>
2022-02-10 01:27:10 +0000

    her

--- weather_app/lib/services/location.dart
+++ weather_app/lib/services/location.dart
@@ -4,42 +4,5 @@ class Location {
   double? latitude;
   double? longitude;
 
-  Future<void> getCurrentLocation() async {
-    try {
-      // Future<Position> _determinePosition() async {
-      //   bool serviceEnabled;
-      //   LocationPermission permission;
-      //   serviceEnabled = await Geolocator.isLocationServiceEnabled();
-      //   if (!serviceEnabled) {
-      //     await Geolocator.openLocationSettings();
-      //     return Future.error('Location services are disabled.');
-      //   }
-      //   permission = await Geolocator.checkPermission();
-      //   if (permission == LocationPermission.denied) {
-      //     permission = await Geolocator.requestPermission();
-      //     if (permission == LocationPermission.denied) {
-      //       return Future.error('Location permissions are denied');
-      //     }
-      //   }
-      //   if (permission == LocationPermission.deniedForever) {
-      //     return Future.error(
-      //         'Location permissions are permanently denied, we cannot request permissions.');
-      //   }
-      //   return await Geolocator.getCurrentPosition(
-      //       forceAndroidLocationManager: false,
-      //       desiredAccuracy: LocationAccuracy.best);
-      // }
-
-      // Position position = await _determinePosition();
-
-      // Position position = await Geolocator.getCurrentPosition(
-      //     forceAndroidLocationManager: true,
-      //     desiredAccuracy: LocationAccuracy.best);
-
-      // latitude = position.latitude;
-      // longitude = position.longitude;
-    } catch (e) {
-      print(e);
-    }
-  }
+  Future<void> getCurrentLocation() async {}
 }
