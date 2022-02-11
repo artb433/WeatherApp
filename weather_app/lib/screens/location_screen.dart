aa4e831a4e331086a770253987c23e6b01528327
Art b <94374097+artb433@users.noreply.github.com>
2022-02-11 23:24:26 +0000

    her

--- weather_app/lib/screens/location_screen.dart
+++ weather_app/lib/screens/location_screen.dart
@@ -23,7 +23,7 @@ class _LocationScreenState extends State<LocationScreen> {
       body: Container(
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('weather_app/images/location_background.jpg'),
+            image: AssetImage('images/location_background.jpg'),
             fit: BoxFit.cover,
             colorFilter: ColorFilter.mode(
                 Colors.white.withOpacity(0.8), BlendMode.dstATop),
